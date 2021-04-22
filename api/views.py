1387dc830ea8fd7952f9f2939b4427398101a920
maidnmw <maidnmw@ya.ru>
2021-04-22 17:44:41 +0500

    main cycle model, click endpoint, index page

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import models
+
 
 # Create your views here.
+def index(request):
+    maincycle = models.MainCycle.objects.first()
+    return render(request, 'index.html', {'maincycle': maincycle})
+
+@api_view(['GET'])
+def call_click(request):
+    maincycle = models.MainCycle.objects.first()
+    maincycle.click()
+    maincycle.save()
+
+    return Response(maincycle.click_count)
---
+++ frontend/static/js/index.js
@@ -0,0 +1,14 @@
+function click() {
+    console.log('click')
+    // const counter = document.getElementById('counter')
+    // counter.innerText = 123
+    // fetch('/api/call_click/', {
+    //     method: 'GET'
+    // }).then(response => {
+    //     console.log(response)
+    //     if (response.ok) return response.json()
+    //     else return Promise.reject(response)
+    // }).then(data => {
+    //     console.log(data)
+    // }).catch(err => console.log(err))
+}
