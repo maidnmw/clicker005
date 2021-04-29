c95c7a4257cd40c412d5068e64342964e165532c
maidnmw <maidnmw@ya.ru>
2021-04-29 17:34:45 +0500

    user auth & main cycle model + user field

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
     path('api/call_click/', views.call_click),
 ]
--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,15 +1,11 @@
-    function call_click() {
-        console.log('click')
-    const counter = document.getElementById('counter')
-    counter.innerText = parseInt(counter.innerText) + 1
-    
-    // fetch('/api/call_click/', {
-    //     method: 'GET'
-    // }).then(response => {
-    //     console.log(response)
-    //     if (response.ok) return response.json()
-    //     else return Promise.reject(response)
-    // }).then(data => {
-    //     console.log(data)
-    // }).catch(err => console.log(err))
+function call_click() {
+    fetch('/api/call_click/', {
+        method: 'GET'
+    }).then(response => {
+        if (response.ok) return response.json()
+        else return Promise.reject(response)
+    }).then(data => {
+        const counter = document.getElementById('counter')
+        counter.innerText = data
+    }).catch(err => console.log(err))
 }
